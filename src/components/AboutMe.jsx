f6edbc2a9aa879ec879a5e0b4b4b439c1d27867e
Mikhail <elkinms85@gmail.com>
2025-01-23 23:59:50 +0200

    add JSON.parse, JSON.stringify to AboutMe module

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,6 +3,9 @@ import {useEffect, useState} from "react";
 
 const aboutMe = () => {
     const [aboutHero, setAboutHero] = useState({});
+    const about_hero = sessionStorage.getItem('about_hero')
+    ? JSON.parse(sessionStorage.getItem('about_hero'))
+    : null;
 
     const getAboutMe = async () => {
         const response = await fetch(`${baseURL}/v1/peoples/1`);
@@ -13,13 +16,22 @@ const aboutMe = () => {
             image: data.image,
             height: data.height
         });
-        sessionStorage.setItem('about_hero', aboutHero);
+        sessionStorage.setItem('about_hero', JSON.stringify({
+            name: data.name,
+            gender: data.gender,
+            image: data.image,
+            height: data.height
+        }));
     }
 
     useEffect(() => {
-        const about_hero = sessionStorage.getItem('about_hero');
-        if (about_hero) {
-            setAboutHero(about_hero);
+        if (about_hero && Object.keys(about_hero) > 0) {
+            setAboutHero({
+                name: about_hero.name,
+                gender: about_hero.gender,
+                image: about_hero.image,
+                height: about_hero.height
+            });
         } else {
             getAboutMe();
         }

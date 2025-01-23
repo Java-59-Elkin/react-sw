2feee330ec6dcd71a727a6a35ba82e818f9b3be4
Mikhail <elkinms85@gmail.com>
2025-01-23 23:25:51 +0200

    add api request to AboutMe. Start to add sessionStorage.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,38 @@
-const AboutMe = () => {
+import {baseURL} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+
+const aboutMe = () => {
+    const [aboutHero, setAboutHero] = useState({});
+
+    const getAboutMe = async () => {
+        const response = await fetch(`${baseURL}/v1/peoples/1`);
+        const data = await response.json();
+        setAboutHero({
+            name: data.name,
+            gender: data.gender,
+            image: data.image,
+            height: data.height
+        });
+        sessionStorage.setItem('about_hero', aboutHero);
+    }
+
+    useEffect(() => {
+        const about_hero = sessionStorage.getItem('about_hero');
+        if (about_hero) {
+            setAboutHero(about_hero);
+        } else {
+            getAboutMe();
+        }
+    }, []);
+
     return (
-        <div>
-            About me
+        <div className="farGalaxy">
+            <img src={`${baseURL}/${aboutHero.image}`} alt="Luke"/>
+            <p>Name: {aboutHero.name}</p>
+            <p>Gender: {aboutHero.gender}</p>
+            <p>Height: {aboutHero.height} sm</p>
         </div>
     );
 };
 
-export default AboutMe;
+export default aboutMe;
